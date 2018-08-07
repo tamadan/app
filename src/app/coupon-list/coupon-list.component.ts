affce40d4e4622255405df402ec240d81d2ceeab
tomohidemiya <t.miya19890131@gmail.com>
2018-08-08 07:37:10 +0900

    こいつ、動くぞ!?

--- src/app/coupon-list/coupon-list.component.ts
+++ src/app/coupon-list/coupon-list.component.ts
@@ -4,6 +4,7 @@ import { StoreInfoComponent} from '../store-info/store-info.component';
 import { CouponListService} from '../coupon-list.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import 'rxjs/add/operator/map';
+import * as anime from 'animejs';
 
 @Component({
   selector: 'app-coupon-list',
@@ -70,5 +71,199 @@ export class CouponListComponent implements OnInit {
   // }
   doFab() {
 		alert('あ');
-	}
+  }
+  
+  clickWarif() {
+    console.log(this.el.nativeElement.getElementsByClassName('swiper-slide-left')[0].style);
+    console.log(this.el.nativeElement.getElementsByClassName('swiper-slide-prev')[0].style);
+    
+    // TODO: どんなブラウザでも真ん中に来るように自動計算しないといけない
+    anime.timeline().add({
+      targets: 'div.card--material.swiper-slide:nth-child(2)',
+      easing: 'easeOutCubic',
+      translateX: [
+
+        {
+          value: -200 ,
+          duration: 400
+        }
+
+      ],
+      translateY: [
+        {
+          value: 4,
+          duration: 100
+        },
+        {
+          value: 8,
+          duration: 50
+        },
+        {
+          value: 15,
+          duration: 250
+        }
+      ],
+      opacity: [1, 0],
+      loop: false
+    }).add({
+      targets: 'div.card--material.swiper-slide:nth-child(3)',
+      offset: '-=1000',
+      easing: 'easeOutCubic',
+      translateX: [
+
+        {
+          value: -15,
+          duration: 400
+        }
+
+      ],
+      translateY: [
+        {
+          value: 1,
+          duration: 100
+        },
+        {
+          value: 2,
+          duration: 50
+        },
+        {
+          value: 8,
+          duration: 250
+        }
+      ],
+      zIndex: {
+        value: [2, 1],
+        round: true
+      },
+      loop: false
+    }).add({
+      targets: 'div.swiper-slide-active',
+      offset: '-=900',
+      easing: 'easeOutCubic',
+      translateX: [
+        
+        {
+          value: -121,
+          duration: 400
+        }
+
+      ],
+      translateY: [
+        {
+          value: 2,
+          duration: 100
+        },
+        {
+          value: 3,
+          duration: 50
+        },
+        {
+          value: 10,
+          duration: 250
+        }
+      ],
+      zIndex: {
+        value: [3, 2],
+        round: true
+      },
+      loop: false
+    }).add({
+      targets: 'div.card--material.swiper-slide:nth-child(5)',
+      offset: '-=800',
+      easing: 'easeOutCubic',
+      translateX: [
+
+        {
+          value: -117,
+          duration: 400
+        }
+
+      ],
+      translateY: [
+        {
+          value: -10,
+          duration: 100
+        },
+        {
+          value: -12,
+          duration: 50
+        },
+        {
+          value: -15,
+          duration: 250
+        }
+      ],
+      zIndex: {
+        value: [2, 3],
+        round: true
+      },
+      loop: false
+    }).add({
+      targets: 'div.card--material.swiper-slide:nth-child(6)',
+      offset: '-=800',
+      easing: 'easeOutCubic',
+      translateX: [
+
+        {
+          value: -15,
+          duration: 400
+        }
+
+      ],
+      translateY: [
+        {
+          value: -10,
+          duration: 100
+        },
+        {
+          value: -12,
+          duration: 50
+        },
+        {
+          value: -15,
+          duration: 250
+        }
+      ],
+      zIndex: {
+        value: [1, 2],
+        round: true
+      },
+      loop: false
+    }).add({
+      targets: 'div.card--material.swiper-slide:nth-child(7)',
+      offset: '-=800',
+      easing: 'easeOutCubic',
+      translateX: [
+
+        {
+          value: -15,
+          duration: 400
+        }
+
+      ],
+      translateY: [
+        {
+          value: -8,
+          duration: 100
+        },
+        {
+          value: -15,
+          duration: 50
+        },
+        {
+          value: -18,
+          duration: 250
+        }
+      ],
+      zIndex: {
+        value: [0, 1],
+        round: true
+      },
+      opacity: 1,
+      loop: false
+    });
+
+    // TODO ここにDOMを操作する処理を入れる
+
+  }
 }
